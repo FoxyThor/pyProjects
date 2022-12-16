51d7fc5791f03ead5b8f76d39b865ae0fe266f12
FoxyThor <119723648+FoxyThor@users.noreply.github.com>
2022-12-16 01:05:38 -0800

    Add files via upload

---
+++ numGame2.py
@@ -0,0 +1,19 @@
+import random
+
+answer = random.randint(1,50)
+
+print('Guess a number between 1 and 50')
+
+guess = int(input('Take a guess. '))
+
+while(guess<50):
+    
+    if(guess == answer):
+        print('You win!')
+        break
+    elif(guess < answer):
+        print('Too low!')
+        guess = int(input('Guess again '))
+    elif(guess > answer):
+        print('Too high!')
+        guess = int(input('Guess again '))
